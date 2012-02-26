64152b6b194b9ceda91fe5233b813022ed5b5c64
Corwin Mcknight <lioneltabre@gmail.com>
2012-02-25 23:54:44 -0800

    Removed useless files, added gitignore.

--- main.c
+++
@@ -1,6 +0,0 @@
-void kmain( void* mbd, unsigned int magic )
-{
-	if ( magic != 0x2BADB002 )
-	{
-	}
-}
--- make.sh
+++ make.sh
@@ -1,3 +1,4 @@
+
 CARGS="-Wall -Wextra -Werror -nostdlib -fno-builtin -nostartfiles -nodefaultlibs"
 clear
 echo Compiling...
@@ -26,4 +27,4 @@ cd ../
 cp kernel.app ./iso/System/Library/CoreServices/Kernel/Kernel.app
 grub-mkrescue -o bootable.iso iso
 echo -------Booting-----------------------------------------
-kvm -cdrom bootable.iso -s -S
+kvm -cdrom bootable.iso
