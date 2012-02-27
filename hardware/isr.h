0a2975318e63aa4d9f02fd174abf36dda1d4f6a7
Corwin Mcknight <lioneltabre@gmail.com>
2012-02-26 19:30:40 -0800

    IRQ Support

--- hardware/isr.h
+++ hardware/isr.h
@@ -1,5 +1,23 @@
 #include "../lib/global.h"
-
+#define IRQ0 32
+#define IRQ1 33
+#define IRQ2 34
+#define IRQ3 35
+#define IRQ4 36
+#define IRQ5 37
+#define IRQ6 38
+#define IRQ7 39
+#define IRQ8 40
+#define IRQ9 41
+#define IRQ10 42
+#define IRQ11 43
+#define IRQ12 44
+#define IRQ13 45
+#define IRQ14 46
+#define IRQ15 47
+typedef void(*isr_t)(registers_t);
+void register_interrupt_handler(u8int n, isr_t handler);
+isr_t interrupt_handlers[256];
 typedef struct registers
 {
    u32int ds;                  // Data segment selector
