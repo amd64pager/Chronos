607a4a8793a4f13fe459df9c53afb1ff2ea2d942
Corwin Mcknight <lioneltabre@gmail.com>
2012-03-03 19:42:41 -0800

    Cleanup

--- hardware/timer/timer.h
+++
@@ -1,8 +0,0 @@
-#ifndef TIMER_H
-#define TIMER_H
-
-#include "../../lib/global.h"
-
-void init_timer(u32int frequency);
-
-#endif
--- screen/vsprintf.h
+++
@@ -1,8 +0,0 @@
-#ifndef VSPRINTF_H
-#define VSPRINTF_H
-
-#include "../lib/stdarg.h"
-
-int vsprintf(char *buf, const char *fmt, va_list args);
-
-#endif
