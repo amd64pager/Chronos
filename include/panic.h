162d181d05ee837119f5efbfc347356d75a097bc
Corwin Mcknight <lioneltabre@gmail.com>
2012-03-07 21:33:55 -0800

    Updated panic.h for stability

--- include/panic.h
+++ include/panic.h
@@ -1,5 +1,28 @@
 #include <screen/printk.h>
-inline void panic(const char* str)
+#ifndef GLOBAL_H
+typedef unsigned int   u32int;
+typedef          int   s32int;
+typedef unsigned short u16int;
+//typedef          short s16int;
+//typedef unsigned char  u8int;
+//typedef          char  s8int;
+#endif
+u16int *video_memory = (u16int*)0xB8000;
+inline void panic(const char* str, int ku)
 {
-	printk("->[Kernel Panic]:Panic! %s",str);
+	if(!ku)
+	{
+		printk("##[Kernel Panic]:Panic! %s",str);
+	}
+	else
+	{
+		printk("KU[Kernel Panic]:Panic! %s",str);
+		__fallback();
+	}
+}
+void __fallback()
+{
+	asm("cli");
+	asm("hlt");
+	while(true);;
 }
